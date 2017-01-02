57dff11772f9ecde58e78dec7d91f0c96c6c3579
kyungwon.kang <kyungwon.kang@bankwareglobal.com>
2017-01-02 19:32:50 +0900

    test

--- src/albert/kang/module01/HelloWorld2.java
+++ src/albert/kang/module01/HelloWorld2.java
@@ -17,7 +17,7 @@ public class HelloWorld2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World!");
+		System.out.println("Hello World2!");
 
 	}
 
