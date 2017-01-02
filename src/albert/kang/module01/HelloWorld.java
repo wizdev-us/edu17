088d58ef8423b016594c5af9dd0341b1def2a83b
kyungwon.kang <kyungwon.kang@bankwareglobal.com>
2017-01-02 17:59:00 +0900

    import

---
+++ src/albert/kang/module01/HelloWorld.java
@@ -0,0 +1,24 @@
+package albert.kang.module01;
+
+/**
+ *
+ *
+ * @author Albert
+ * @history
+ * 2017. 1. 2.         initial creation
+ */
+public class HelloWorld {
+
+	/**
+	 *
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!");
+
+	}
+
+}
