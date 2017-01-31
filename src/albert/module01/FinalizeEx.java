71c887ecb123c01dc8a786ec9939a649d422c76b
kyungwon.kang <kyungwon.kang@bankwareglobal.com>
2017-01-31 13:05:46 +0900

    1

--- src/albert/module01/FinalizeEx.java
+++ src/albert/module01/FinalizeEx.java
@@ -1,28 +1,28 @@
-package albert.module00;
+package albert.module01;
 
 public class FinalizeEx {
-	
+
 	String objName;
+
 	public FinalizeEx(String name) {
 		objName = name;
 	}
-	
-	protected void finalize() throws Throwable {
-		super.finalize();
-		System.out.println(objName + "이 소멸되었습니다.");
-	}
-	
-	
+
 	public static void main(String[] args) {
 		FinalizeEx obj1 = new FinalizeEx("inst01");
 		FinalizeEx obj2 = new FinalizeEx("inst02");
-		
-		obj1 = null;
-		obj2 = null;
-		
+
+		// obj1 = null;
+		// obj2 = null;
+
 		System.out.println("프로그램을 종료합니다.");
-		
+
 		System.gc();
-		//System.runFinalization();
+		// System.runFinalization();
+	}
+
+	protected void finalize() throws Throwable {
+		super.finalize();
+		System.out.println(objName + "이 소멸되었습니다.");
 	}
 }
--- src/albert/module02/FullQualification.java
+++
@@ -1,7 +0,0 @@
-package albert.module02;
-
-public class FullQualification {
-	public static void main(String[] args) {
-		java.util.ArrayList list = new java.util.ArrayList();
-	}
-}
